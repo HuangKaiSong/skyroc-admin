ac0111bf5fc57d72314b3e2ae77459dcde6e4232
Ohh-889 <1509326266@qq.com>
2025-12-10 23:55:07 +0800

    feat(unocss): add @sa/uno-preset and related presets, transformers, and theme settings for enhanced styling capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { setupI18n } from './locales';
 import { setupAppVersionNotification } from './plugins/app';
+import './plugins/assets';
+import { setupIconifyOffline } from './plugins/iconify';
 
 function setupApp() {
   const container = document.getElementById('root');
@@ -14,6 +16,8 @@ function setupApp() {
   root.render(<App />);
 
   setupI18n();
+
+  setupIconifyOffline();
 
   setupAppVersionNotification();
 }
