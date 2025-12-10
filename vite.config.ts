e072de20913f3b17172843104a67a6913366e6d5
Ohh-889 <1509326266@qq.com>
2025-12-10 21:41:04 +0800

    feat(vite): enhance build output with custom asset & chunk paths

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,59 @@ export default defineConfig(configEnv => {
   const buildTime = getBuildTime();
 
   return {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: chunkInfo => {
+            const name = chunkInfo.names[0];
+
+            if (name?.endsWith('.css')) {
+              return 'css/[name]-[hash].css';
+            }
+
+            const imgExts = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp'];
+
+            if (imgExts.some(ext => name?.endsWith(`.${ext}`))) {
+              return 'images/[name]-[hash].[ext]';
+            }
+
+            if (name?.endsWith('.js')) {
+              return 'js/[name]-[hash].js';
+            }
+
+            return 'assets/[name]-[hash].[ext]';
+          },
+          chunkFileNames: chunkInfo => {
+            // 检查文件路径，如果是 pages 目录下的文件，则修改文件名和路径
+            const filePath = chunkInfo.facadeModuleId;
+
+            if (filePath) {
+              // 提取文件的父文件夹作为文件名
+              if (filePath.includes('/src/pages/')) {
+                // 提取文件的父文件夹作为文件名
+                const pageName = filePath.split('/src/pages/')[1];
+                // 替换 [name] 为  name 因为vite不支持
+                const newPath = pageName.replace(/\[([^\]]+)\]/g, '$1');
+
+                const path = newPath.slice(0, newPath.lastIndexOf('/'));
+
+                return `js/pages/${path}/[name]-[hash].js`;
+              } else if (filePath.includes('/src/components/')) {
+                return `js/components/[name]-[hash].js`;
+              }
+            }
+
+            return 'js/[name]-[hash].js'; // 默认处理方式
+          },
+          manualChunks: {
+            antd: ['antd'],
+            il8n: ['react-i18next', 'i18next'],
+            react: ['react', 'react-dom'],
+            'react-router': ['@tanstack/react-router']
+          }
+        }
+      }
+    },
     define: {
       BUILD_TIME: JSON.stringify(buildTime)
     },
