86ee14feadd5045878b927dfb2b1f81ba665c9fa
Ohh-889 <1509326266@qq.com>
2025-12-12 09:44:36 +0800

    feat(router): add detailed comments for TanStack Router config

--- src/features/router/index.ts
+++ src/features/router/index.ts
@@ -2,6 +2,14 @@ import { createRouter } from '@tanstack/react-router';
 
 import { routeTree } from './routeTree.gen';
 
+/**
+ * - 路由配置
+ * - 使用的是 TanStack Router 的 约定式路由配置方法 无需手动配置路由
+ * - 路由配置文件为 src/features/router/routeTree.gen.ts
+ * - 路由约定式 [详情见](https://tanstack.com/router/latest/docs/framework/react/routing/routing-concepts)
+ * - https://tanstack.com/router/latest/docs/framework/react/routing/file-based-routing
+ * - https://tanstack.com/router/latest/docs/framework/react/routing/file-naming-conventions
+ */
 export const router = createRouter({
   context: {},
   defaultPreload: 'intent',
