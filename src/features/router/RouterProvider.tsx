79dcb461c6013dcacaa4225640defcf8d3c0170c
Ohh-889 <1509326266@qq.com>
2025-12-16 13:23:43 +0800

    feat(project): integrate auth context into RouterProvider

--- src/features/router/RouterProvider.tsx
+++ src/features/router/RouterProvider.tsx
@@ -1,9 +1,18 @@
 import { RouterProvider as TanStackRouterProvider } from '@tanstack/react-router';
 
+import { useAuth } from '../auth/use-auth';
+
 import { router } from '.';
 
 const RouterProvider = () => {
-  return <TanStackRouterProvider router={router} />;
+  const { isLoggedIn, token } = useAuth();
+
+  return (
+    <TanStackRouterProvider
+      context={{ token, isLoggedIn }}
+      router={router}
+    />
+  );
 };
 
 export default RouterProvider;
