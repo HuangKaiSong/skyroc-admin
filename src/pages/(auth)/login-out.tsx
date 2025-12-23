c22155d4636cf5683c1a6ebd5fbf4ff93274e4da
Ohh-889 <1509326266@qq.com>
2025-12-23 17:05:13 +0800

    feat(project): add logout redirect

--- src/pages/(auth)/login-out.tsx
+++ src/pages/(auth)/login-out.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 const LoginOut = () => {
   return null;
@@ -9,5 +9,8 @@ export const Route = createFileRoute('/(auth)/login-out')({
   staticData: {
     title: 'login-out',
     i18nKey: 'route.login-out'
+  },
+  beforeLoad: () => {
+    throw redirect({ to: '/login' });
   }
 });
