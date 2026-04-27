f0d400c1aa439ada0e0e6f33252219c6c22444e1
Ohh-889 <1509326266@qq.com>
2026-04-27 11:35:02 +0800

    refactor(accordion): remove unused module, clean styles

--- apps/web-ui-playground/src/app/[locale]/(demo)/accordion/modules/AccordionCustom.tsx
+++ apps/web-ui-playground/src/app/[locale]/(demo)/accordion/modules/AccordionCustom.tsx
@@ -35,7 +35,7 @@ const AccordionCustom = () => {
       classNames={{
         content: 'px-3 leading-8',
         item: 'border-b-0',
-        trigger: `mb-2 rounded-md px-3 text-left underline-offset-2 data-[state=closed]:(bg-muted/50 no-underline) data-[state=open]:(bg-secondary-foreground/20 underline hover:bg-secondary-foreground/20 hover:underline) hover:bg-muted`
+        trigger: `mb-2 rounded-md px-3 text-left underline-offset-2 data-[state=closed]:(bg-muted/50 no-underline) data-[state=open]:(bg-secondary-foreground/20 underline hover:underline) hover:bg-muted`
       }}
     />
   );
