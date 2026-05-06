0633ba0cbb37d2349c147beb61af15bd21320122
Ohh-889 <1509326266@qq.com>
2026-05-06 16:33:22 +0800

    refactor(state): reorganize exports and streamline type declarations for improved clarity

--- packages/@core/state/src/index.ts
+++ packages/@core/state/src/index.ts
@@ -5,11 +5,14 @@ export type { JotaiProviderProps } from './provider/JotaiProvider';
 // Store
 export { getAtomValue, globalStore, setAtomValue, updateAtomValue } from './store/global';
 
+// Types
+export type { AtomStorage } from './types';
 // Utils
 export { atomWithPartial } from './utils/atom-with-partial';
 export type { PartialUpdater } from './utils/atom-with-partial';
 export { createAtomWithStorage } from './utils/atom-with-storage';
 export type { CreateAtomWithStorageOptions } from './utils/atom-with-storage';
+
 export {
   __clearStorageRegistry,
   getStorage,
@@ -17,6 +20,3 @@ export {
   registerStorage,
   unregisterStorage
 } from './utils/storage-registry';
-
-// Types
-export type { AtomStorage } from './types';
