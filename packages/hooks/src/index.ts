2a420305a34944bb2a26e648202b356e27b020dd
Ohh-889 <1509326266@qq.com>
2026-05-07 18:27:04 +0800

    refactor(hooks): swap useNow and useCountDownTimer exports

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -4,8 +4,8 @@ export type { Subscribable } from './store';
 
 // 纯 React hooks（平台无关）
 export { default as useArray } from './use-array';
-export { useNow } from './use-now';
+export { default as useCountDownTimer } from './use-count-down-timer';
 
 // ahooks 封装 hooks（平台无关）
 export { default as useLoading } from './use-loading';
-export { default as useCountDownTimer } from './use-count-down-timer';
+export { useNow } from './use-now';
