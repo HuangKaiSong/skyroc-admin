896af0ddd95001703ebc50ada5a454eabb9c1fc3
Ohh-889 <1509326266@qq.com>
2026-05-09 01:11:51 +0800

    refactor(admin): remove isPC

--- apps/admin/src/utils/storage.ts
+++ apps/admin/src/utils/storage.ts
@@ -1,9 +1,5 @@
-import { createLocalforage, createStorage } from '@skyroc/utils';
+import { createStorage } from '@skyroc/utils';
 
 const storagePrefix = import.meta.env.VITE_STORAGE_PREFIX || '';
 
 export const localStg = createStorage<StorageType.Local>('local', storagePrefix);
-
-export const sessionStg = createStorage<StorageType.Session>('session', storagePrefix);
-
-export const localforage = createLocalforage<StorageType.Local>('local');
